f53efe9912ce89555f16dbf71d4fbb5b4ff93ce8
Gaojianli <823805669hehe@gmail.com>
2021-01-14 21:24:18 +0800

    spilt tails

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,51 @@
 <template>
   <h1>{{ msg }}</h1>
   <p>Please input your hand cards:</p>
-  <input v-model="message" placeholder="edit me"/>
-  <button @click="showHandCards">OK</button>
-  <p>you handCards: {{ handCards }}</p>
+  <input v-model="handCards" placeholder="手牌"/>
+  <button @click="showHandCards">新規</button>
+  <p>手牌: {{ handCards }}</p>
 </template>
 
 <script>
+const tiles = {
+    m:["🀇","🀈","🀉","🀊","🀋","🀌","🀍","🀎","🀏"],
+    p:["🀙","🀚","🀛","🀜","🀝","🀞","🀟","🀠","🀡"],
+    s:["🀐","🀑","🀒","🀓","🀔","🀕","🀖","🀗","🀘"],
+    z:["🀀","🀁","🀂","🀃","🀆","🀅","🀄","🀫","🀪"]
+};
 export default {
-  handCards: '',
-  name: 'HelloWorld',
   props: {
     msg: String
   },
   data() {
     return {
-      message:"",
+        handCards: '',
     }
   },
   methods:{
-    inputOK(){
-      alert(this.message)//24m1556p2459s4572z
-    },
     showHandCards(){
-      var regValue = /[a-z]/;
-      let handCards = this.message.split(regValue);
-      alert(handCards);
+      const tails = this.splitTiles(this.handCards);
+      console.log(tails);
+    },
+    splitTiles(handCard){
+      const result={
+        p:[],
+        m:[],
+        s:[],
+        z:[]
+      }
+      console.log(this.handCards);
+      let rest = handCard;
+      while(rest.length>0){
+        debugger;
+        const colored= rest.match(/[0-9]*[spzm]/);
+        const matchStr=colored[0];
+        result[matchStr[matchStr.length-1]]=matchStr.substr(0,matchStr.length-1).split('');
+        rest=rest.substr(0,colored.index)+rest.substr(colored.index+matchStr.length);
+      }
+      console.log(result);
+      return result;
     }
-
   }
 }
 </script>
