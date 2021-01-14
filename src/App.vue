6e22258c00daf81082dd492d88f71547c2685669
Pixy Qi <pixy.qi@gmail.com>
2021-01-14 20:35:02 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/majhong.png" style="transform:scale(0.7)" />
+  <HelloWorld msg="Welcome to Pixy Majhong website" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>Please input your hand cards:</p>
+  <input v-model="message" placeholder="edit me"/>
+  <button @click="showHandCards">OK</button>
+  <p>you handCards: {{ handCards }}</p>
+</template>
+
+<script>
+export default {
+  handCards: '',
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      message:"",
+    }
+  },
+  methods:{
+    inputOK(){
+      alert(this.message)//24m1556p2459s4572z
+    },
+    showHandCards(){
+      var regValue = /[a-z]/;
+      let handCards = this.message.split(regValue);
+      alert(handCards);
+    }
+
+  }
+}
+</script>
+
